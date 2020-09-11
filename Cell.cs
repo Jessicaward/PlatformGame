68a4eeee25f837e5b6a4b61c6e4dd4f15013d9a0
Fergus Coulter <ferguscoulter@MacBook-Pro.lan>
2020-09-11 21:00:11 +0100

    asset addition and start of generation

---
+++ Cell.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+namespace PlatformGame
+{
+    public class Cell
+    {
+        private SquareType type;
+        private int xCoord;
+        private int yCoord;
+        private bool visited;
+
+        public SquareType type()
+        {
+            get { return type; }
+        }
+        public int xCoord()
+        {
+
+        }
+
+        public Cell()
+        {
+
+        }
+    }
+}
--- Level.cs
+++ Level.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace PlatformGame
 {
@@ -23,16 +24,19 @@ namespace PlatformGame
             {
                 case Difficulty.Smol:
                 default:
-                    map = new SquareType[64, 64];
+                    map = Generate(new SquareType[64,64]);
                     pickupsRemaining = 6;
+                    
                     break;
                 case Difficulty.Reggi:
                     map = new SquareType[256, 256];
                     pickupsRemaining = 25;
+                    Generate(map);
                     break;
                 case Difficulty.Chonky:
                     map = new SquareType[512, 512];
                     pickupsRemaining = 50;
+                    Generate(map);
                     break;
             }
         }
@@ -42,6 +46,56 @@ namespace PlatformGame
         public void Update(GameTime gameTime)
         {
             //remove coins here etc
+        }
+
+        public SquareType[,] Generate(SquareType [,] mapArray)
+        {
+            mapArray = InitialiseMaze(mapArray);
+
+            bool check = false;
+
+            int xIndex = 0;
+            int yIndex = 1;
+
+
+
+
+            do
+            {
+                Walk(xIndex, yIndex, mapArray);
+
+
+
+            } while (!check);
+
+            
+        }
+
+        private int[] Walk(int x, int y, SquareType[,] map)
+        {
+            bool check2 = false;
+
+            Random rnd = new Random();
+
+
+
+        }
+
+        private SquareType[,] InitialiseMaze(SquareType[,] map)
+        {
+
+
+            for(var x = 0; x < map.GetLength(0); x++)
+            { 
+                for (var y = 0; y < map.GetLength(1); y++)
+                {
+                    map[x, y] = SquareType.Wall;
+                }
+            }
+
+            map[0, 1] = SquareType.Floor;
+
+            return map;
         }
     }
 }
