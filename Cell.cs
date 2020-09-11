a6b709f09873f9816fb154ac9341a4ed4d93f320
Jessica Ward <Jessicawardiom@gmail.com>
2020-09-11 21:05:12 +0100

    Fixed build

--- Cell.cs
+++ Cell.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.Xna.Framework;
-namespace PlatformGame
+﻿namespace PlatformGame
 {
     public class Cell
     {
@@ -9,13 +7,19 @@ namespace PlatformGame
         private int yCoord;
         private bool visited;
 
-        public SquareType type()
+        public SquareType Type
         {
             get { return type; }
         }
-        public int xCoord()
-        {
 
+        public int XCoord
+        {
+            get { return xCoord; }
+        }
+
+        public int YCoord
+        {
+            get { return yCoord; }
         }
 
         public Cell()
