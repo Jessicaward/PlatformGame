6ed552462ba390c06196d1a2f5e4676b1adf09a3
Jessica Ward <Jessicawardiom@gmail.com>
2020-09-11 17:17:14 +0100

    Added level class to get SquareType for specified square

---
+++ Level.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PlatformGame
+{
+    public class Level
+    {
+        public static SquareType GetSquareType(float x, float y)
+        {
+            //todo: implement this lmao, it needs to check the map at the x and y points
+            return SquareType.Floor;
+        }
+    }
+}
